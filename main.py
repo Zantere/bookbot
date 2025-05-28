ac5f873c6edd5ebf0fbb6a7e63ef5146d3563de0
Zantere <m.zantere@gmail.com>
2025-05-28 11:23:14 +0900

    refactor

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-get_book_test()
-    
+from stats import count_words
+
+def main() :
+    book_content = get_book_test("./books/frankenstein.txt")
+    text = get_book_test("./books/frankenstein.txt")
+    word_count = count_words(text)
+    print(f"{num_words} words found in the document")
+
+#    print(book_content)
+
+def get_book_test(filepath):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        return f.read()
+
+if __name__ == "__main__":
+    main()
